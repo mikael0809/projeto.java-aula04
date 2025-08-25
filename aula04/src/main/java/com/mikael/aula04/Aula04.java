37e001f7f6a7beb0478cdf7f411ed8bb48e8e05e
Mikael <mikael.marques@duque.g12.br>
2025-08-25 19:40:31 -0300

    Primeiro commit da aplicação

---
+++ aula04/src/main/java/com/mikael/aula04/Aula04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mikael.aula04;
+
+/**
+ *
+ * @author 08270
+ */
+public class Aula04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
