c3e1926f9b23c01a199f2f8d871fe59dfeb67ecd
Mikael <mikael.marques@duque.g12.br>
2025-08-25 19:46:25 -0300

    Primeiro commit da aplicação

--- aula04/src/main/java/com/mikael/aula04/Aula04.java
+++ aula04/src/main/java/com/mikael/aula04/Aula04.java
@@ -11,6 +11,8 @@ package com.mikael.aula04;
 public class Aula04 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int a = 4;
+        int b = 6;
+        int soma = a+b;
     }
 }
